e909e3f79cebf663b844a2a0a8e670dc8cfdabde
Mahendra Choudhary <pikaatic@gmail.com>
2020-07-24 06:39:43 +0530

    add IIFE and constants

---
+++ app.js
@@ -0,0 +1,23 @@
+;(function () {
+  const body = document.querySelector('body')
+  const button = document.querySelector('button')
+  const h1 = document.querySelector('h1')
+  const hexValues = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ]
+})()
